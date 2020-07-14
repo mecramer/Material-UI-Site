f2a4505696f3cd490411a1a9063eceaf40191eb7
Mark Cramer <mecramer@users.noreply.github.com>
2020-07-14 18:16:30 -0400

    Adding routes to mobile apps and websites pages

--- src/components/App.js
+++ src/components/App.js
@@ -8,6 +8,8 @@ import Footer from './ui/Footer'
 import LandingPage from './LandingPage'
 import Services from './Services'
 import CustomSoftware from './CustomSoftware'
+import MobileApps from './MobileApps'
+import Websites from './Websites'
 
 function App () {
     // hook for active menu index number
@@ -45,8 +47,20 @@ function App () {
                             <CustomSoftware {...props} setValue={setValue} setSelectedIndex={setSelectedIndex} />
                         )}
                     />
-                    <Route exact path='/mobileapps' component={() => <div>Mobile Apps test</div>} />
-                    <Route exact path='/websites' component={() => <div>Websites test</div>} />
+                    <Route
+                        exact
+                        path='/mobileapps'
+                        render={(props) => (
+                            <MobileApps {...props} setValue={setValue} setSelectedIndex={setSelectedIndex} />
+                        )}
+                    />
+                    <Route
+                        exact
+                        path='/websites'
+                        render={(props) => (
+                            <Websites {...props} setValue={setValue} setSelectedIndex={setSelectedIndex} />
+                        )}
+                    />
                     <Route exact path='/revolution' component={() => <div>Revolution test</div>} />
                     <Route exact path='/about' component={() => <div>About test</div>} />
                     <Route exact path='/contact' component={() => <div>Contact test</div>} />
