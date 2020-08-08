2b3001dd524e42c4cfc312ef976511842bf63be5
Mark Cramer <mecramer@users.noreply.github.com>
2020-08-07 20:19:42 -0400

    Adding new page routes

--- src/components/App.js
+++ src/components/App.js
@@ -12,6 +12,7 @@ import MobileApps from './MobileApps'
 import Websites from './Websites'
 import Revolution from './Revolution'
 import About from './About'
+import Contact from './Contact'
 
 function App () {
     // hook for active menu index number
@@ -21,61 +22,20 @@ function App () {
     return (
         <ThemeProvider theme={theme}>
             <BrowserRouter>
-                <Header
-                    value={value}
-                    setValue={setValue}
-                    selectedIndex={selectedIndex}
-                    setSelectedIndex={setSelectedIndex}
-                />
+                <Header value={value} setValue={setValue} selectedIndex={selectedIndex} setSelectedIndex={setSelectedIndex} />
                 <Switch>
-                    <Route
-                        exact
-                        path='/'
-                        render={(props) => (
-                            <LandingPage {...props} setValue={setValue} setSelectedIndex={setSelectedIndex} />
-                        )}
-                    />
-                    <Route
-                        exact
-                        path='/services'
-                        render={(props) => (
-                            <Services {...props} setValue={setValue} setSelectedIndex={setSelectedIndex} />
-                        )}
-                    />
+                    <Route exact path='/' render={(props) => <LandingPage {...props} setValue={setValue} setSelectedIndex={setSelectedIndex} />} />
+                    <Route exact path='/services' render={(props) => <Services {...props} setValue={setValue} setSelectedIndex={setSelectedIndex} />} />
                     <Route
                         exact
                         path='/customsoftware'
-                        render={(props) => (
-                            <CustomSoftware {...props} setValue={setValue} setSelectedIndex={setSelectedIndex} />
-                        )}
+                        render={(props) => <CustomSoftware {...props} setValue={setValue} setSelectedIndex={setSelectedIndex} />}
                     />
-                    <Route
-                        exact
-                        path='/mobileapps'
-                        render={(props) => (
-                            <MobileApps {...props} setValue={setValue} setSelectedIndex={setSelectedIndex} />
-                        )}
-                    />
-                    <Route
-                        exact
-                        path='/websites'
-                        render={(props) => (
-                            <Websites {...props} setValue={setValue} setSelectedIndex={setSelectedIndex} />
-                        )}
-                    />
-                    <Route
-                        exact
-                        path='/revolution'
-                        render={(props) => (
-                            <Revolution {...props} setValue={setValue} setSelectedIndex={setSelectedIndex} />
-                        )}
-                    />
-                    <Route
-                        exact
-                        path='/about'
-                        render={(props) => <About {...props} setValue={setValue} setSelectedIndex={setSelectedIndex} />}
-                    />
-                    <Route exact path='/contact' component={() => <div>Contact test</div>} />
+                    <Route exact path='/mobileapps' render={(props) => <MobileApps {...props} setValue={setValue} setSelectedIndex={setSelectedIndex} />} />
+                    <Route exact path='/websites' render={(props) => <Websites {...props} setValue={setValue} setSelectedIndex={setSelectedIndex} />} />
+                    <Route exact path='/revolution' render={(props) => <Revolution {...props} setValue={setValue} setSelectedIndex={setSelectedIndex} />} />
+                    <Route exact path='/about' render={(props) => <About {...props} setValue={setValue} setSelectedIndex={setSelectedIndex} />} />
+                    <Route exact path='/contact' render={(props) => <Contact {...props} setValue={setValue} setSelectedIndex={setSelectedIndex} />} />
                     <Route exact path='/estimate' component={() => <div>Estimate test</div>} />
                 </Switch>
                 <Footer setValue={setValue} setSelectedIndex={setSelectedIndex} />
