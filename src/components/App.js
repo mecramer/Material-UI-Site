dc9524ba2cb06e4a0ba07f3a8bd91873b1219161
Mark Cramer <mecramer@users.noreply.github.com>
2020-08-15 11:13:28 -0400

    Adding estimate page path

--- src/components/App.js
+++ src/components/App.js
@@ -13,6 +13,7 @@ import Websites from './Websites'
 import Revolution from './Revolution'
 import About from './About'
 import Contact from './Contact'
+import Estimate from './Estimate'
 
 function App () {
     // hook for active menu index number
@@ -24,19 +25,51 @@ function App () {
             <BrowserRouter>
                 <Header value={value} setValue={setValue} selectedIndex={selectedIndex} setSelectedIndex={setSelectedIndex} />
                 <Switch>
-                    <Route exact path='/' render={(props) => <LandingPage {...props} setValue={setValue} setSelectedIndex={setSelectedIndex} />} />
-                    <Route exact path='/services' render={(props) => <Services {...props} setValue={setValue} setSelectedIndex={setSelectedIndex} />} />
+                    <Route
+                        exact
+                        path='/'
+                        render={(props) => <LandingPage {...props} setValue={setValue} setSelectedIndex={setSelectedIndex} />}
+                    />
+                    <Route
+                        exact
+                        path='/services'
+                        render={(props) => <Services {...props} setValue={setValue} setSelectedIndex={setSelectedIndex} />}
+                    />
                     <Route
                         exact
                         path='/customsoftware'
                         render={(props) => <CustomSoftware {...props} setValue={setValue} setSelectedIndex={setSelectedIndex} />}
                     />
-                    <Route exact path='/mobileapps' render={(props) => <MobileApps {...props} setValue={setValue} setSelectedIndex={setSelectedIndex} />} />
-                    <Route exact path='/websites' render={(props) => <Websites {...props} setValue={setValue} setSelectedIndex={setSelectedIndex} />} />
-                    <Route exact path='/revolution' render={(props) => <Revolution {...props} setValue={setValue} setSelectedIndex={setSelectedIndex} />} />
-                    <Route exact path='/about' render={(props) => <About {...props} setValue={setValue} setSelectedIndex={setSelectedIndex} />} />
-                    <Route exact path='/contact' render={(props) => <Contact {...props} setValue={setValue} setSelectedIndex={setSelectedIndex} />} />
-                    <Route exact path='/estimate' component={() => <div>Estimate test</div>} />
+                    <Route
+                        exact
+                        path='/mobileapps'
+                        render={(props) => <MobileApps {...props} setValue={setValue} setSelectedIndex={setSelectedIndex} />}
+                    />
+                    <Route
+                        exact
+                        path='/websites'
+                        render={(props) => <Websites {...props} setValue={setValue} setSelectedIndex={setSelectedIndex} />}
+                    />
+                    <Route
+                        exact
+                        path='/revolution'
+                        render={(props) => <Revolution {...props} setValue={setValue} setSelectedIndex={setSelectedIndex} />}
+                    />
+                    <Route
+                        exact
+                        path='/about'
+                        render={(props) => <About {...props} setValue={setValue} setSelectedIndex={setSelectedIndex} />}
+                    />
+                    <Route
+                        exact
+                        path='/contact'
+                        render={(props) => <Contact {...props} setValue={setValue} setSelectedIndex={setSelectedIndex} />}
+                    />
+                    <Route
+                        exact
+                        path='/estimate'
+                        render={(props) => <Estimate {...props} setValue={setValue} setSelectedIndex={setSelectedIndex} />}
+                    />
                 </Switch>
                 <Footer setValue={setValue} setSelectedIndex={setSelectedIndex} />
             </BrowserRouter>
